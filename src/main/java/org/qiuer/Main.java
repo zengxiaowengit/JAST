8ebf2bcf11d8146ef22364e1eb56e4935827abce
xiaoxiaowen <zengxiaowenhit@126.com>
2019-04-02 23:19:12 +0800

    优化代码

--- src/main/java/org/qiuer/Main.java
+++ src/main/java/org/qiuer/Main.java
@@ -16,10 +16,9 @@ import java.util.Map;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    File file = new File("D:\\github\\AST\\src\\main\\resources\\ast.json");
-    FileInputStream stream;
+    InputStream stream = ASTParser.class.getResourceAsStream("/ast.json");
     String encoding = "UTF-8";
-    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+    BufferedReader reader = new BufferedReader(new InputStreamReader(stream, encoding));
     StringBuilder sb = new StringBuilder();
     String line = "";
     while((line = reader.readLine()) != null){
